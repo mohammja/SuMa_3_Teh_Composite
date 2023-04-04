4f280de7eff9333b60b2a7b624880c5156323d53
mohammja <mohammja@metropolia.fi>
2023-04-04 16:09:31 +0300

    composite pattern is edited so now all components type is Hardware as Simo asked

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         //This was the first composite pattern
-    /*    Motherboard motherBoard = new Motherboard(1000);
-        Processor processor = new Processor(600);
-        NetworkCard networkCard = new NetworkCard(50);
-        GraphicsCard graphicsCard = new GraphicsCard(800);
-        Memory memory = new Memory(300);
+        Motherboard motherBoard = new Motherboard(1000);
+        Hardware processor = new Processor(600);
+        Hardware networkCard = new NetworkCard(50);
+        Hardware graphicsCard = new GraphicsCard(800);
+        Hardware memory = new Memory(300);
 
         motherBoard.addComponents(processor);
         motherBoard.addComponents(graphicsCard);
@@ -16,34 +16,34 @@ public class Main {
 
         Case myCase = new Case(120);
 
-        DesktopComputer myComputer = new DesktopComputer(motherBoard,myCase);
-        System.out.println("Total price for this PC: " + myComputer.getPrice() + " €");*/
+        DesktopComputer myComputer = new DesktopComputer(motherBoard, myCase);
+        System.out.println("Total price for this PC: " + myComputer.getPrice() + " €");
 
-
-        //This is the composite pattern using abstract factory
-        HardwareFactory factoryA = new FactoryA();
-        HardwareFactory factoryB = new FactoryB();
-
-        Case firstCase = factoryA.createCase();
-
-        Motherboard firstMB = factoryA.createMotherboard();
-        firstMB.addComponents(factoryA.createMemory());
-        firstMB.addComponents(factoryA.createGraphicsCard());
-        firstMB.addComponents(factoryA.createProcessor());
-        firstMB.addComponents(factoryA.createNetworkCard());
-
-        DesktopComputer firstPC = new DesktopComputer(firstMB, firstCase);
-        System.out.println("Total price if you buy from the factory A: " + firstPC.getPrice() + " €");
-
-        Case secondCase = factoryB.createCase();
-
-        Motherboard secondMB = factoryB.createMotherboard();
-        secondMB.addComponents(factoryB.createMemory());
-        secondMB.addComponents(factoryB.createGraphicsCard());
-        secondMB.addComponents(factoryB.createProcessor());
-        secondMB.addComponents(factoryB.createNetworkCard());
-
-        DesktopComputer secondPC = new DesktopComputer(secondMB, secondCase);
-        System.out.println("Total price if you buy from the factory B: " + secondPC.getPrice() + " €");
+//
+//        //This is the composite pattern using abstract factory
+//        HardwareFactory factoryA = new FactoryA();
+//        HardwareFactory factoryB = new FactoryB();
+//
+//        Hardware firstCase = factoryA.createCase();
+//
+//        Motherboard firstMB = factoryA.createMotherboard();
+//        firstMB.addComponents(factoryA.createMemory());
+//        firstMB.addComponents(factoryA.createGraphicsCard());
+//        firstMB.addComponents(factoryA.createProcessor());
+//        firstMB.addComponents(factoryA.createNetworkCard());
+//
+//        DesktopComputer firstPC = new DesktopComputer(firstMB, (Case) firstCase);
+//        System.out.println("Total price if you buy from the factory A: " + firstPC.getPrice() + " €");
+//
+//        Hardware secondCase = factoryB.createCase();
+//
+//        Motherboard secondMB = factoryB.createMotherboard();
+//        secondMB.addComponents(factoryB.createMemory());
+//        secondMB.addComponents(factoryB.createGraphicsCard());
+//        secondMB.addComponents(factoryB.createProcessor());
+//        secondMB.addComponents(factoryB.createNetworkCard());
+//
+//        DesktopComputer secondPC = new DesktopComputer(secondMB, (Case) secondCase);
+//        System.out.println("Total price if you buy from the factory B: " + secondPC.getPrice() + " €");
     }
 }
